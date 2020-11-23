77c2d0aa2869ce30cc2d148fca06bdcc53b099eb
Nebye <nebyeb@hotmail.com>
2020-11-22 17:04:09 -0700

    just initial set up

---
+++ connect6_viz.py
@@ -0,0 +1,39 @@
+# Just in case I don't get the othello viz/player working on time, I'm going to make this
+# This is a viz for Connect6 (which is a valid game for the CGO games comp so it should be valid for this assignment)
+
+import math
+import numpy as np
+# import random
+
+
+
+
+def play(move):
+    if (move == 0):
+        player = 'black'
+    else:
+        player = 'white'
+        
+    location = input("What is your play "+player+" [format:x y]: ") 
+    
+    #while(location != valid):
+    #    location = input("Invalid move, please try again: ")
+        
+    board(location)
+
+def board(last_play):
+    print(board_size)
+    
+def main():
+    # first = random.randint(0,1) # 0 is black and 1 is white
+    # according to rules black always plays first
+    first = 0
+    
+    global board_size
+    board_size = int(input("What is your board size (min of 19): "))
+    while(board_size < 19):
+        board_size = int(input("What is your board size (min of 19): "))
+    play(first)
+    
+
+main()
